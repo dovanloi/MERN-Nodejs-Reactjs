627260edd0b2852c10d7b8fe4cbbd737f69ca692
dovanloi <49943861+dovanloi@users.noreply.github.com>
2019-07-09 22:04:34 +0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://admin:Loikthp@cluster0-6t2iw.mongodb.net/test?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://admin:password@cluster0-6t2iw.mongodb.net/test?retryWrites=true&w=majority',
   secretOrKey: 'secret'
 };
